d6b448bb6a98ed9b365a2058606bdf9323078c52
Jan Mankopf <jan.mankopf@gmx.de>
2020-01-02 15:48:35 +0100

    init project

---
+++ webpack.production.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    hashDigestLength: 6,
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].chunk.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.(vert|frag)$/,
+        use: "raw-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html", title: "WebGL Particles" })
+  ]
+};
